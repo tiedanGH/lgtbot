3d1d89550c61eb9ce37d5c3a514c8711bf135a2d
tiedanGH <2295824927@qq.com>
2026-02-06 23:02:54 -0500

    numcomb: fix seed option bug & add text status command

--- games/opencomb/options.h
+++ games/opencomb/options.h
@@ -1,5 +1,5 @@
 EXTEND_OPTION("每回合超时时间x秒", 局时, (ArithChecker<uint32_t>(10, 3600, "局时（秒）")), 120)
-EXTEND_OPTION("随机种子（关联卡池&道具，配置需一致）", 种子, (AnyArg("种子", "我是随便输入的一个字符串")), "")
+EXTEND_OPTION("随机种子（关联卡池&道具，配置需一致）", 种子, (OptionalDefaultChecker<AnyArg>("", "种子", "我是随便输入的一个字符串")), "")
 EXTEND_OPTION("游戏卡池：决定卡池中砖块的种类", 卡池, AlterChecker<uint32_t>({{"经典", 0}, {"癞子", 1}, {"空气", 2}, {"混乱", 3}}), 0)
 EXTEND_OPTION("游戏模式：游戏采用的基础放置和对战规则", 模式, AlterChecker<uint32_t>({{"传统", 0}, {"云顶", 1}}), 0)
 EXTEND_OPTION("开启连线额外得分（长度3及以上时获得额外分数）", 连线奖励, (BoolChecker("开启", "关闭")), true)
